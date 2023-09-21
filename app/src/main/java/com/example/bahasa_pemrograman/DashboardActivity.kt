cfebb0727db78191e804fe01947e8503392e05d2
rabihutomo <rabihutomo11@gmail.com>
2023-09-21 15:58:44 +0700

    init project

---
+++ app/src/main/java/com/example/bahasa_pemrograman/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bahasa_pemrograman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
